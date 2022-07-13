421f8ad96fe0f95409899a5d5a8f3c5114286512
Pelmenis4 <matiss.j.strauts@gmail.com>
2022-07-13 06:02:20 +0300

    styling on header and dashboard

--- src/app/dashboard/dashboard-document/dashboard-document.component.ts
+++ src/app/dashboard/dashboard-document/dashboard-document.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { DocumentService } from 'src/app/shared/document.service';
 
@@ -7,11 +7,13 @@ import { DocumentService } from 'src/app/shared/document.service';
   templateUrl: './dashboard-document.component.html',
   styleUrls: ['./dashboard-document.component.css']
 })
-export class DashboardDocumentComponent implements OnInit {
+export class DashboardDocumentComponent implements OnInit, AfterViewInit {
 
   @Input() document
   @Input() index: number;
   @Input() currentPage: number;
+  @ViewChild('border') divElementRef: ElementRef;
+  borderElement: HTMLElement;
 
   constructor(private router: Router, private documentService: DocumentService) { }
 
@@ -21,9 +23,25 @@ export class DashboardDocumentComponent implements OnInit {
     // translates the decisionAction.
     this.decisionAction = this.documentService.translateDecisionActionToLatvian(this.document.decisionAction);
   }
+
+  ngAfterViewInit() {
+    this.bottomBorder();
+  }
   
   // navigates to /documents/id route. The id is the index of the document that was selected.
   onDocument() {
     this.router.navigate(['/document', this.currentPage - 9 + this.index]);
+  }
+
+  // very bad for now, will improve later.
+  bottomBorder() {
+    if(this.documentService.documents.length > 10 && this.index < 8) {
+      this.borderElement = this.divElementRef.nativeElement;
+      this.borderElement.classList.add('border-bottom');
+    }
+    else if(this.index < this.documentService.documents.length - 1) {
+      this.borderElement = this.divElementRef.nativeElement;
+      this.borderElement.classList.add('border-bottom');
+    }
   }
 }
