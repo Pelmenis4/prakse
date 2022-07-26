05a920112527a079208369c76a1c398a7042b4d8
Pelmenis4 <matiss.j.strauts@gmail.com>
2022-07-26 13:02:56 +0300

    Using one document instead of all the documents in document-view-data.

--- src/app/document-view/document-view-data/document-view-data.component.ts
+++ src/app/document-view/document-view-data/document-view-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DocumentService } from 'src/app/shared/document.service';
 import { getCCBNote } from 'src/app/shared/RequiredData';
@@ -11,7 +11,7 @@ import { getCCBNote } from 'src/app/shared/RequiredData';
 export class DocumentViewDataComponent implements OnInit {
 
   id: number;
-  documents = [];
+  document;
   statusMessage: string;
   buttonEnabled: boolean = true;
 
@@ -30,64 +30,48 @@ export class DocumentViewDataComponent implements OnInit {
   
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'] - 1;  //gets the id of the document from the url
-    this.documents = this.documentService.documents;
-    this.decisionAction = this.documentService.translateDecisionActionToLatvian(this.documents[this.id].decisionAction); // translates the decisionAction
+    this.document = this.documentService.documents[this.id];
+    this.decisionAction = this.documentService.translateDecisionActionToLatvian(this.document.decisionAction); // translates the decisionAction
     this.decisionActionCaps = this.documentService.documents[this.id].decisionAction;
     this.decisionActionMessage = getCCBNote(this.decisionActionCaps, this.documentService.documents[this.id].values, this.decisionAction)
-    console.log(this.decisionActionMessage);
     this.changeStatus();
   }
 
 
 // Translates the values from the document from english to latvian.
   changeStatus() {
-    if (this.documents[this.id].status == "DIGITIZED")
+    if (this.document.status == "DIGITIZED")
       this.statusMessage = "Digitalizēts"
-    else if (this.documents[this.id].status == "SENT_TO_CCB")
+    else if (this.document.status == "SENT_TO_CCB")
       this.statusMessage = "Nosūtīts uz CCB"
     else
       this.statusMessage = "Not found"
   }
 
-  // loads all the data from the selected document
-  // loadData() {
-  //   this.decisionAction = this.documentService.translateDecisionActionToLatvian(this.documents[this.id].decisionAction);
-    // this.personalCode = this.documents[this.id].personalCode;
-    // this.name = this.documents[this.id].name;
-    // this.caseNumber = this.documents[this.id].caseNumber;
-    // this.location = this.documents[this.id].location;
-    // this.date = this.documents[this.id].date;
-    // this.contractNumber = this.documents[this.id].contractNumber;
-    // this.contractDate = this.documents[this.id].contractDate;
-    // this.debtAmount = this.documents[this.id].debtAmount;
-    // this.debtCurrency = this.documents[this.id].debtCurrency;
-    // this.court = this.documents[this.id].court;
-    // this.decision = this.documents[this.id].decision;
-  // }
   
 
   // changes decisionAction based on which radio button is selected.
   onDecision(action: string) {
-    this.documents[this.id].decisionAction = this.documentService.translateDecisionActionToEnglish(action);
+    this.document.decisionAction = this.documentService.translateDecisionActionToEnglish(action);
   }
 
   // changes status when the "Sūtīt uz CCB button" is pressed.
   onChangeStatus() {
-    this.documents[this.id].status = "SENT_TO_CCB";
+    this.document.status = "SENT_TO_CCB";
     this.changeStatus();
     this.buttonEnabled = false;
   }
 
   // conditions to disable the 'Sūtīt uz CCB' button.
   disableButton() {
-    if(this.documents[this.id].personalCode == '' 
-    || this.documents[this.id].name == ''
-    || this.documents[this.id].caseNumber== ''
-    || this.documents[this.id].location == ''
-    || this.documents[this.id].contractNumber == ''
-    || this.documents[this.id].debtAmount == null
-    || this.documents[this.id].court == ''
-    || this.documents[this.id].decision == ''
+    if(this.document.personalCode == '' 
+    || this.document.name == ''
+    || this.document.caseNumber== ''
+    || this.document.location == ''
+    || this.document.contractNumber == ''
+    || this.document.debtAmount == null
+    || this.document.court == ''
+    || this.document.decision == ''
     || this.statusMessage == 'Nosūtīts uz CCB'
     || this.buttonEnabled == false){
       return true;
